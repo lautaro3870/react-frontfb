5462a3bce23549cf664aa531be087bc35ae54788
lautaro3870 <lautarodisbro@gmail.com>
2023-04-13 16:47:51 +0200

    De todo un poco

---
+++ src/components/Table.jsx
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from "react";
+import DataTable, { createTheme } from "react-data-table-component";
+import { AiFillPrinter, AiOutlineEdit } from "react-icons/ai";
+import { BiTrash } from "react-icons/bi";
+
+export default function Table() {
+  const [proyectos, setProyectos] = useState([]);
+
+  const url = "https://proyecto-fundacion.herokuapp.com/api/Proyecto";
+  const token =
+    "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyMSIsIm5iZiI6MTY4MTIwMTU2MiwiZXhwIjoxNjg1NTIxNTYyLCJpYXQiOjE2ODEyMDE1NjJ9.nelLW9XLyoHp7yHvTtNhyxY4O0AoFM5EBzY6HLmggzxzHUF-3Yqe1FawpQFb09_6qnJrGocD3QAffYsYoPjPLw";
+
+  const getProyectos = async () => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    setProyectos(data);
+  };
+
+  useEffect(() => {
+    getProyectos();
+  }, []);
+
+  const columns = [
+    {
+      name: "Title",
+      selector: (row) => row.titulo,
+    },
+    {
+      name: "Year",
+      selector: (row) => row.anioInicio,
+    },
+    {
+      name: "Areas",
+      selector: (row) => row.listaAreas.map((i) => i.area1 + ", "),
+    },
+    {
+      name: "Actions",
+      selector: (row) => (
+        <div>
+          <button>
+            <BiTrash />
+          </button>
+          <br />
+          <button>
+            <AiOutlineEdit />
+          </button>
+          <br />
+          <button
+            onClick={() => {
+              alert(row.id);
+            }}
+          >
+            <AiFillPrinter />
+          </button>
+        </div>
+      ),
+    },
+  ];
+
+  const handleSort = (column, sortDirection) =>
+    console.log(column.selector, sortDirection);
+
+  createTheme(
+    "custom",
+    {
+      text: {
+        primary: "#268bd2",
+        secondary: "#2aa198",
+      },
+      background: {
+        default: "#002b36",
+      },
+      context: {
+        background: "#cb4b16",
+        text: "#FFFFFF",
+      },
+      divider: {
+        default: "#073642",
+      },
+      action: {
+        button: "rgba(0,0,0,.54)",
+        hover: "rgba(0,0,0,.08)",
+        disabled: "rgba(0,0,0,.12)",
+      },
+    },
+    "dark"
+  );
+
+  return (
+    <div>
+      <DataTable
+        fixedHeader
+        fixedHeaderScrollHeight="500px"
+        columns={columns}
+        theme="dark"
+        data={proyectos}
+        pagination
+        onSort={handleSort}
+      />
+    </div>
+  );
+}
